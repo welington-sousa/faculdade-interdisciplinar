802b4e145cc94556fabd70cdaaf58f7932e9b87a
Welington Sousa <welingtonwork@gmail.com>
2016-05-06 10:18:15 -0300

    Validator para caso usuáio já exista retorna uma mensagem

--- src/main/java/br/com/camisascriativas/controllers/UsuariosController.java
+++ src/main/java/br/com/camisascriativas/controllers/UsuariosController.java
@@ -7,6 +7,7 @@ import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.validator.SimpleMessage;
 import br.com.caelum.vraptor.validator.Validator;
 import br.com.camisascriativas.daos.UsuarioDao;
 import br.com.camisascriativas.models.Usuario;
@@ -14,9 +15,12 @@ import br.com.camisascriativas.models.Usuario;
 @Controller
 @Path("/usuarios")
 public class UsuariosController {
-	@Inject private UsuarioDao dao;
-	@Inject private Validator validator;
-	@Inject private Result result;
+	@Inject
+	private UsuarioDao dao;
+	@Inject
+	private Validator validator;
+	@Inject
+	private Result result;
 
 	@Get("/novo")
 	public void novo() {
@@ -24,6 +28,7 @@ public class UsuariosController {
 
 	@Post("")
 	public void adiciona(Usuario usuario) {
+		validator.addIf(dao.existe(usuario), new SimpleMessage("Login já existe", "usuario.login"));
 		dao.salva(usuario);
 		validator.onErrorUsePageOf(this).novo();
 
