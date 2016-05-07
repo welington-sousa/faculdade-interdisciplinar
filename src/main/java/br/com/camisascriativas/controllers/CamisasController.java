b9c526f923d296f68f9eddb668e6e2ac66a08465
Welington Sousa <welingtonwork@gmail.com>
2016-05-07 15:20:36 -0300

    Refatorando controller

--- src/main/java/br/com/camisascriativas/controllers/CamisasController.java
+++ src/main/java/br/com/camisascriativas/controllers/CamisasController.java
@@ -12,36 +12,20 @@ import br.com.camisascriativas.annotation.RestrictMethod;
 import br.com.camisascriativas.daos.CamisaDao;
 import br.com.camisascriativas.models.Camisa;
 
-@Controller
-@Path("/camisas")
+@Controller @Path("/camisas")
 public class CamisasController {
-	@Inject
-	private CamisaDao dao;
-	@Inject
-	private Result result;
-
-	@Get("")
-	public List<Camisa> camisas() {
-		return this.dao.listaTudo();
-	}
-
-	public void femininas() {
-		this.result.redirectTo(this).camisas();
-	}
-
-	public void masculinas() {
-		this.result.redirectTo(this).camisas();
-	}
 	
-	public void criancas() {
-		this.result.redirectTo(this).camisas();
-	}
+	@Inject private CamisaDao dao;
+	@Inject private Result result;
 
-	@RestrictMethod
-	@Get("/{id}")
-	public Camisa produto(Long id) {
-		return this.dao.buscaPorId(id);
-	}
+	@Get("") public List<Camisa> camisas() {return this.dao.listaTudo();}
+
+	public void femininas() {this.result.redirectTo(this).camisas();}
+	public void masculinas() {this.result.redirectTo(this).camisas();}
+	public void criancas() {this.result.redirectTo(this).camisas();}
+	
+	@RestrictMethod	@Get("/{id}")
+	public Camisa produto(Long id) {return this.dao.buscaPorId(id);}
 	
 	public List<Camisa> busca(String nome) {
 		return dao.buscaCamisasPor(nome);
