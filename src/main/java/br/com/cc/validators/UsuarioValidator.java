ece8f1c89107d1324492fa06e5d635e7a1a4c85f
Welington Sousa <welingtonwork@gmail.com>
2016-05-07 15:38:32 -0300

    Refatorando Validations

--- src/main/java/br/com/cc/validators/UsuarioValidator.java
+++ src/main/java/br/com/cc/validators/UsuarioValidator.java
@@ -12,12 +12,9 @@ public class UsuarioValidator {
 	@Inject private Validator validator;
 	@Inject private UsuarioDao dao;
 
-	public void validate(Usuario usuario) {
-		validator.validate(usuario);
-
-		if (dao.existeUsuarioComLogin(usuario)) {
+	public void validate(Usuario usuario) { validator.validate(usuario);
+		if (dao.existeUsuarioComLogin(usuario)) 
 			validator.add(new I18nMessage("usuario.login", "ja.existe"));
-		}
 	}
 
 	public <T> T onErrorRedirectTo(T controller) {
