610460437806c7e1235858809d6f7aa8c6d9dd5f
Welington Sousa <welingtonwork@gmail.com>
2016-05-07 15:37:45 -0300

    Injetando dependencia LoginValidatorn

--- src/main/java/br/com/camisascriativas/controllers/LoginController.java
+++ src/main/java/br/com/camisascriativas/controllers/LoginController.java
@@ -9,6 +9,7 @@ import br.com.caelum.vraptor.Result;
 import br.com.camisascriativas.daos.UsuarioDao;
 import br.com.camisascriativas.models.Usuario;
 import br.com.camisascriativas.models.UsuarioLogado;
+import br.com.cc.validators.LoginValidator;
 
 @Controller
 public class LoginController {
@@ -16,11 +17,14 @@ public class LoginController {
 	@Inject private UsuarioLogado logado;
 	@Inject private UsuarioDao dao;
 	@Inject private Result result;
+	@Inject private LoginValidator validator;
 
 	@Get("login/autentica") public void loginForm() {}
 
 	@Post("/autentica")
 	public void autentica(Usuario usuario) {
+		validator.validate(usuario);
+		validator.onErrorRedirectTo(this).loginForm();
 		logado.login(dao.carrega(usuario.getLogin()));
 		result.redirectTo(CamisasController.class).camisas();
 	}
