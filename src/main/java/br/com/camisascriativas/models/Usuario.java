b4b0d7048e527f68e7d64567929baf77a2237f30
Welington Sousa <welingtonwork@gmail.com>
2016-05-07 14:49:45 -0300

    Refatorando class Usuario

--- src/main/java/br/com/camisascriativas/models/Usuario.java
+++ src/main/java/br/com/camisascriativas/models/Usuario.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 
 @Entity
 public class Usuario implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,35 +20,15 @@ public class Usuario implements Serializable {
 	private String senha;
 	private String email;
 
-	public String getEmail() {
-		return email;
-	}
+	public void setEmail(String email) {this.email = email;}
+	public String getEmail() {return email;}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	public void setNome(String nome) {this.nome = nome;}
+	public String getNome() {return nome;}
 
-	public String getNome() {
-		return nome;
-	}
+	public void setLogin(String login) {this.login = login;}
+	public String getLogin() {return login;}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+	public void setSenha(String senha) {this.senha = senha;}
+	public String getSenha() {return senha;}
 }
