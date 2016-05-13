ddb906cc350946d81eeb65938a5936b33d85ed45
Welington Sousa <welingtonwork@gmail.com>
2016-05-13 16:49:20 -0300

    Removendo código de busca desnecessário e inserindo link para página de efetivar

---
+++ src/main/java/br/com/camisascriativas/controllers/ComprasController.java
@@ -0,0 +1,18 @@
+package br.com.camisascriativas.controllers;
+
+import javax.inject.Inject;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Result;
+
+@Controller @Path("/compras")
+public class ComprasController {
+	
+	@Inject private Result result;
+	
+	@Get("/efetivar") public void efetivar() {}
+
+	public void compra() {result.include("msg", "Sucesso");}
+}
