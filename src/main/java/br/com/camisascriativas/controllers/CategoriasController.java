3593017aee493e1da309bafed439e130cbf83798
Welington Sousa <welingtonwork@gmail.com>
2016-05-14 11:51:47 -0300

    Criando lógica de páginação

--- src/main/java/br/com/camisascriativas/controllers/CategoriasController.java
+++ src/main/java/br/com/camisascriativas/controllers/CategoriasController.java
@@ -1,5 +1,21 @@
 package br.com.camisascriativas.controllers;
 
-public class CategoriasController {
+import javax.inject.Inject;
 
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Result;
+import br.com.camisascriativas.daos.CategoriaDao;
+
+public class CategoriasController {
+	
+	@Inject private CategoriaDao categoriaDao;
+	@Inject private Result result;
+
+	@Get("")
+	public void list(Integer page) {
+		if (page == null) {
+			page = 0;
+		}
+		result.include("paginatedList", categoriaDao.paginated(page, 10));
+	}
 }
