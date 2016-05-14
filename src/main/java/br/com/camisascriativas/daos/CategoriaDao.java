3e56b94b84c10d6dab88c2f42b3bbd83ee6164c9
Welington Sousa <welingtonwork@gmail.com>
2016-05-14 11:30:54 -0300

    Criando classe CategoriaDao

---
+++ src/main/java/br/com/camisascriativas/daos/CategoriaDao.java
@@ -0,0 +1,18 @@
+package br.com.camisascriativas.daos;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.camisascriativas.models.Categoria;
+
+public class CategoriaDao {
+
+	@Inject private EntityManager manager;
+
+	public List<Categoria> listaTudo() {
+		return manager.createQuery("from Categoria c", Categoria.class)
+				.getResultList();
+	}
+}
